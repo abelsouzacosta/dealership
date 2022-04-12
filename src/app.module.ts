77a04c0cbd71c06781db636d54ac739aee80a48b
Abel Junior <abel.costa@webjump.com.br>
2022-04-12 11:06:09 -0300

    feat(cars): create
    allows o create a car in the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CarsModule } from './cars/cars.module';
 import 'dotenv/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,61 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
+import { Model } from 'mongoose';
+import { Car } from './entities/car.entity';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class CarsService {
+  constructor(@InjectModel('Car') private readonly carModel: Model<Car>) {}
+
+  private async throwsExceptionIfLicensePlateIsAlreadyRegistered(
+    license_plate: string,
+  ) {
+    const licensePlateFound = await this.carModel.findOne({
+      license_plate,
+    });
+
+    if (licensePlateFound)
+      throw new HttpException(
+        `License plate already registered`,
+        HttpStatus.CONFLICT,
+      );
+  }
+
+  async create({
+    brand,
+    model,
+    color,
+    fabrication_year,
+    model_year,
+    license_plate,
+  }: CreateCarDto) {
+    await this.throwsExceptionIfLicensePlateIsAlreadyRegistered(license_plate);
+
+    await this.carModel.create({
+      brand,
+      model,
+      color,
+      fabrication_year,
+      model_year,
+      license_plate,
+    });
+  }
+
+  findAll() {
+    return `This action returns all cars`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} car`;
+  }
+
+  update(id: number, updateCarDto: UpdateCarDto) {
+    return `This action updates a #${id} car`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} car`;
+  }
+}
