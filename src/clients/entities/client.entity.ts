8d05ff88f1983d426410c99447cbff13a6794597
Abel Junior <abel.costa@webjump.com.br>
2022-04-13 09:59:19 -0300

    feat(clients): adding client entity

---
+++ src/clients/entities/client.entity.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type PhoneNumber = {
+  number: string;
+};
+
+export type Address = {
+  country: string;
+  state: string;
+  city: string;
+  district: string;
+  street: string;
+  number: string;
+  zip_code: string;
+};
+
+@Schema({
+  timestamps: true,
+  collection: 'clients',
+})
+export class Client {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  cpf: string;
+
+  @Prop({ required: true })
+  phone_numbers: PhoneNumber[];
+
+  @Prop({ required: true })
+  addresses: Address[];
+}
+
+export const ClientSchema = SchemaFactory.createForClass(Client);
